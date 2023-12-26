4dca6abd14b0b3ddc952c5ac8de91f377006d93f
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2023-12-26 21:00:36 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.Write("Введите первое число:  ");
+int a=int.Parse(Console.ReadLine());
+Console.Write("Введите второе число:  ");
+int b=int.Parse(Console.ReadLine());
+Console.Write("Введите третье число:  ");
+int c=int.Parse(Console.ReadLine());
+Console.WriteLine("{0}  {1}  {2}",a,b,c);
